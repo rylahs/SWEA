c9f9645ce57142a9ee170fe7ef37aadc7e5d1dff
Chan ho Moon <minsonyo@naver.com>
2022-01-31 02:49:11 +0900

    [SWEA D1]2046. 스탬프 찍기
    [SWEA D1]2046. 스탬프 찍기

---
+++ 0x01/2046.cpp
@@ -0,0 +1,23 @@
+// 0x01. Difficulty Level 1
+// Written by : Rylah
+// Date : 2022.01.31
+// 2046. ½ºÅÆÇÁ Âï±â
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKdT6AyYDFAUq
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		cout << "#";
+	}
+
+	return 0;
+}
