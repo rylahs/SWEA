6010deee193b89efbb2e6643e7c8ff2e8f56191f
Chan ho Moon <minsonyo@naver.com>
2022-01-31 02:48:28 +0900

    [SWEA D12043. 서랍의 비밀번호
    [SWEA D12043. 서랍의 비밀번호

---
+++ 0x01/2043.cpp
@@ -0,0 +1,31 @@
+// 0x01. Difficulty Level 1
+// Written by : Rylah
+// Date : 2022.01.31
+// 2043. 서랍의 비밀번호
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QJ_8KAx8DFAUq
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int p, k;
+	cin >> p >> k;
+	int cnt = 1;
+	while (p != k)
+	{
+		if (p == k)
+		{
+			break;
+		}
+		k++;
+		cnt++;	
+		if (k == 1000)
+			k = 0;
+	}
+	cout << cnt << "\n";
+	return 0;
+}
