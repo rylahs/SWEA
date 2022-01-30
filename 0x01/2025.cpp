ce2651332f6be6c10065a2d874be8a0cfa0ccd28
Chan ho Moon <minsonyo@naver.com>
2022-01-31 02:47:43 +0900

    [SWEA D1]2025. N줄 덧셈
    [SWEA D1]2025. N줄 덧셈

---
+++ 0x01/2025.cpp
@@ -0,0 +1,20 @@
+// 0x01. Difficulty Level 1
+// Written by : Rylah
+// Date : 2022.01.31
+// 2025. N¡Ÿ µ°º¿
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QFZtaAscDFAUq
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	cout << n * (n + 1) / 2 << "\n";
+
+	return 0;
+}
